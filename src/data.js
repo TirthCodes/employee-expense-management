81035998bbbea31a799e905d3d510bafa6d3b0ae
Tirth Patel <tirthpatelcse@gmail.com>
2023-06-19 14:01:57 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,43 @@
+const USERS = [
+  {
+    id: 1,
+    fname: "Max",
+    lname: "Miller",
+  },
+  {
+    id: 2,
+    fname: "David",
+    lname: "Smith",
+  },
+  {
+    id: 3,
+    fname: "Tom",
+    lname: "Holland",
+  }
+];
+
+const EXPENSES = [
+  {
+    id: 1,
+    expense_id: 1,
+    category: "Food",
+    description: "Dominos Pizza",
+    cost: 50
+  },
+  {
+    id: 1,
+    expense_id: 2,
+    category: "Travel",
+    description: "Business Trip",
+    cost: 500
+  },
+  {
+    id: 2,
+    expense_id: 3,
+    category: "Equipment",
+    description: "Home Office Setup",
+    cost: 250
+  },
+]
+
+export { USERS, EXPENSES };
